d97cfc1a5c6db5f529a1785aa4b240c94533d841
Adan Silva <adan.t.silva77@gmail.com>
2023-06-28 23:12:46 -0700

    Add commands and personal info

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,26 +1,31 @@
 var linkedin = "https://www.linkedin.com/in/adan-silva-7939a2273/";
 var github = "https://github.com/secmancer/";
-var tryhackme = "";
-var hackthebox = "";
+var tryhackme = "https://tryhackme.com/p/Adan.Silva";
+var hackthebox = "https://app.hackthebox.com/profile/1505106";
+var reddit = "https://www.reddit.com/user/posemancer/";
 var discord = "secmancer (formerly secmancer:6944)"
 var email = 'mailto:adan.t.silva77@gmail.com';
+var resume = "https://docs.google.com/document/d/1Anyk7aXzhCeQxycVVsqFrxZoH0rLJ53W/edit?usp=sharing&ouid=100723709531946559864&rtpof=true&sd=true";
 
 whois = [
   "<br>",
-  "Hey, I'm Forrest!👋",
-  "I'm a software developer and content creator, who builds engaging websites like this one",
-  "and makes YouTube videos about computer science & software engineering.",
-  "After graduating with a Bachelor's in Computer Science, I worked professionally",
-  "as a software engineer building enterprise web applications for Fortune 500 companies.",
-  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
-  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
-  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
-  "I needed to make the jump from software engineering to full time content creator, and it's",
-  "the best decision I ever made.",
-  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
-  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
-  "because they're reading this right now, is that I also run a creative & media agency.",
-  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "Hello! 👋",
+  "I'm Adan Silva, a 18 year old student who is very interested in the field of cybersecurity.",
+  "Being a recent high school graduate, I have successfully completed my high school's Cybersecurity CTE pathway.",
+  "I was one of the first students to do so and earned my CompTIA ITF+ certificate from that program.",
+  "Currently, I am studying for my Security+ certificate, with plans on earning more from organizations like CompTIA and the EC Council.",
+  "While studying for my certificates, I also work on my skills by playing CTFs through sites like TryHackMe or HackTheBox!",
+  "While I'm not super certain on what to do yet, penetration testing and vulnerability assesments have caught my eye.",
+  "I am currently attending Allan Hancock College as I have taken mutliple dual enrollement classes while in high school.",
+  "As of me writing this, I am waitlisted at Cal Poly SLO for Computer Science. I hope to recieve an acceptance soon!",
+  "I have also been a participate for the FIRST Robotics Competition for 4 years and have been with the organization for 6+.",
+  "As an FIRST alumni, I am currently mentoring my former team in my spare time and where ever I can.",
+  "I have programming experience in C++/C and Java, and I also learning how to use Python and Go.",
+  "While I am not focused on web development, I do have some experience with HTML, CSS, and Java.",
+  "I also have experience working with Git and GitHub, where I was in charge of merging pull requests for a software development team.",
+  "I have experience working with the Windows operating system and comfortable working with Linux systems as well.",
+  "In my freetime, I like playing with my dogs, hanging out with my friends, or listening to the latest Darknet Diaries epsiode!",
+  "Thank you for taking the time to read about me. Feel free to look around this site!",
   "<br>"
 ];
 
@@ -33,20 +38,27 @@ whoami = [
 
 social = [
   "<br>",
-  "Here are some ways to reach me!",
+  "Here are some ways to reach me.",
   "<br>",
   'discord        ' + discord,
+  'reddit         <a href="' + reddit + '" target="_blank">u/posemancer' + "</a>",
   'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/adan-silva-7939a2273' + "</a>",
   'github         <a href="' + github + '" target="_blank">github/secmancer' + "</a>",
+  'tryhackme      <a href="' + tryhackme + '" target="_blank">tryhackme/Adan.Silva' + "</a>",
+  'hackthebox     <a href="' + hackthebox + '" target="_blank">hackthebox/secmancer' + "</a>",
   "<br>",
-  "Hope to hear from you soon.",
+  "Hope to hear from you soon!",
   "<br>",
 ];
 
 projects = [
   "<br>",
-  "Still curating... most projects are offline, on GitHub, or confidential.",
-  "<br>"
+  '<a href="https://github.com/frc3512/Robot-2023">Robot 2023:</a> Used Java and various libraries to develop the code for the 2023 robot.',
+  '<a href="https://github.com/frc3512/Robot-2022">Robot 2022:</a> Used C++ and various libraries to develop the code for the 2022 robot.',
+  '<a href="https://github.com/frc3512/SwerveBot-2022">SwerveBot2022:</a> Used Java and various libraries to develop a soild swerve drive template that a bunch of teams utilize for the 2023 season.',
+  '<a href="https://github.com/frc3512/frc3512.github.io">frc3512.github.io:</a> Used HTML, CSS, and JS to write documentation and tutorials for software member.',
+  '<a href="https://github.com/secmancer/OhWhatFun">OhWhatFun!:</a> Built a Minecraft mod that adds ridable sleds into the world and submitted it for a modjam.',
+  '<a href="https://github.com/secmancer/GameBoy-Picture">GameBoyPicture:</a> Simple GameBoy app I built for a hackathon.',
 ];
 
 help = [
@@ -54,20 +66,21 @@ help = [
   "-- PROFILE COMMANDS --",
   '<span class="command">whois</span>          Who is this Adan Silva?',
   '<span class="command">social</span>         Display ways to reach me',
-  '<span class="command">projects</span>       View all of the projects I have worked on',
+  '<span class="command">projects</span>       Projects I have worked on',
   '<span class="command">reachme</span>        Reach me via Email',
+  '<span class="command">resume</span>         Download my resume!',
   "<br>",
   "-- BASIC COMMANDS --",
   '<span class="command">whoami</span>         Who are you?',
   '<span class="command">history</span>        View command history',
-  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">help</span>           Self explanatory',
   '<span class="command">clear</span>          Clear terminal',
   '<span class="command">banner</span>         Display the header',
   "<br>",
   "-- QUESTIONABLE COMMANDS --",
   '<span class="command">msfconsole</span>     Run the Metasploit framework',
   '<span class="command">sudo -l</span>        Maybe you can run some files?',
-  '<span class="command">sudo rm -rf *</span>  Run the funny command!',
+  '<span class="command">sudo rm -rf /</span>  Run the funny command!',
   "<br>",
 ];
 
--- scripts/main.js
+++ scripts/main.js
@@ -59,36 +59,43 @@ function commander(cmd) {
     case "whoami":
       loopLines(whoami, "color2 margin", 80);
       break;
-    case "video":
-      addLine("Opening YouTube...", "color2", 80);
-      newTab(youtube);
+    case "sudo -l":
+      addLine("Permission denied. Nice try though!");
       break;
     case "sudo":
-      addLine("Oh no, you're not admin...", "color2", 80);
+      addLine("guest is not in the sudoers file.  This incident will be reported.", "color2", 80);
       setTimeout(function() {
-        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        window.open('https://www.youtube.com/watch?v=SxlRhT1lNtU');
+      }, 1000); 
+      break;
+    case "sudo rm -rf /":
+      setTimeout(function() {
+        window.open('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lQpve6-1g5tIDNWYOIzUUAAAAA%26pid%3DApi&f=1&ipt=23a30faa19899d5a86903e767ce0ee311d3c464b413c7e1f62d0f1d12651d7a4&ipo=images');
+      }, 10);
+      addLine("You're lucky this isn't a real Linux machine!");
+      break;
+    case "msfconsole":
+      addLine("msfconsole: unknown module. Not sure what package manager this uses, so good luck on that.");
+      break;
+    case "resume":
+      addLine("Retrieving resume...", "color2", 80);
+      setTimeout(function() {
+        window.open(resume);
       }, 1000); 
       break;
     case "social":
       loopLines(social, "color2 margin", 80);
       break;
-    case "secret":
-      liner.classList.add("password");
-      pw = true;
-      break;
     case "projects":
       loopLines(projects, "color2 margin", 80);
-      break;
-    case "password":
-      addLine("<span class=\"inherit\"> Lol! You're joking, right? You\'re gonna have to try harder than that!😂</span>", "error", 100);
       break;
     case "history":
       addLine("<br>", "", 0);
       loopLines(commands, "color2", 80);
       addLine("<br>", "command", 80 * commands.length + 50);
       break;
-    case "email":
-      addLine('Opening mailto:<a href="mailto:forrest@fkcodes.com">forrest@fkcodes.com</a>...', "color2", 80);
+    case "reachme":
+      addLine('Opening mailto:<a href="mailto:adan.t.silva77@gmail.com">adan.t.silva77@gmail.com</a>...', "color2", 80);
       newTab(email);
       break;
     case "clear":
@@ -101,25 +108,36 @@ function commander(cmd) {
       loopLines(banner, "", 80);
       break;
     // socials
-    case "youtube":
-      addLine("Opening YouTube...", "color2", 80);
-      newTab(youtube);
-      break;
-    case "twitter":
-      addLine("Opening Twitter...", "color2", 0);
-      newTab(twitter);
+    case "discord":
+      addLine("Add me on Discord! Username: secmancer");
+      addLine("Formally known as: secmancer$6944");
       break;
     case "linkedin":
-      addLine("Opening LinkedIn...", "color2", 0);
+      addLine("Opening LinkedIn profile...", "color2", 0);
       newTab(linkedin);
       break;
-    case "instagram":
-      addLine("Opening Instagram...", "color2", 0);
-      newTab(instagram);
-      break;
     case "github":
-      addLine("Opening GitHub...", "color2", 0);
+      addLine("Opening GitHub profile...", "color2", 0);
       newTab(github);
+      break;
+    case "tryhackme":
+      addLine("Opening TryHackMe profile...", "color2", 0);
+      newTab(tryhackme);
+      break;
+    case "hackthebox":
+      addLine("Opening HackTheBox profile...", "color2", 0);
+      newTab(hackthebox);
+      break;
+    case "reddit":
+      addLine("Opening Reddit profile...", "color2", 0);
+      newTab(reddit);
+      break;
+    case "credits":
+      addLine("Huge thanks to ForrestKnight!");
+      addLine("He served as a huge inspiration to me for creating this site.");
+      addLine("This site heavily uses his source code with my own modifications.");
+      addLine("You can find the original source code in the video's description.");
+      addLine('You can watch that video <a href="https://www.youtube.com/watch?v=KtYby2QN0kQ&pp=ygUWZm9ycmVzdGtuaWdodCB0ZXJtaW5hbA%3D%3D)">here.' + "</a>");
       break;
     default:
       addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
