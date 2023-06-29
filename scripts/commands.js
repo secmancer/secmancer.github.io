217e6d4e1f92029b82b35bee5b78c2e3f5b2f9b3
Adan Silva <adan.t.silva77@gmail.com>
2023-06-28 19:05:49 -0700

    Use ForrestKnight's original source code for a terminal website

---
+++ scripts/commands.js
@@ -0,0 +1,101 @@
+var linkedin = "https://www.linkedin.com/in/adan-silva-7939a2273/";
+var github = "https://github.com/secmancer/";
+var tryhackme = "";
+var hackthebox = "";
+var discord = "secmancer (formerly secmancer:6944)"
+var email = 'mailto:adan.t.silva77@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "guest",
+  "Well... DUH!",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  "Here are some ways to reach me!",
+  "<br>",
+  'discord        ' + discord,
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/adan-silva-7939a2273' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/secmancer' + "</a>",
+  "<br>",
+  "Hope to hear from you soon.",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  "-- PROFILE COMMANDS --",
+  '<span class="command">whois</span>          Who is this Adan Silva?',
+  '<span class="command">social</span>         Display ways to reach me',
+  '<span class="command">projects</span>       View all of the projects I have worked on',
+  '<span class="command">reachme</span>        Reach me via Email',
+  "<br>",
+  "-- BASIC COMMANDS --",
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+  "-- QUESTIONABLE COMMANDS --",
+  '<span class="command">msfconsole</span>     Run the Metasploit framework',
+  '<span class="command">sudo -l</span>        Maybe you can run some files?',
+  '<span class="command">sudo rm -rf *</span>  Run the funny command!',
+  "<br>",
+];
+
+banner = [
+"                                ",
+"                            ",
+"                             _______  ______   _______  __    _    _______  ___   ___      __   __  _______ ",
+"  -----------,-|            |   _   ||      | |   _   ||  |  | |  |       ||   | |   |    |  | |  ||   _   |",
+"           ,','|            |  |_|  ||  _    ||  |_|  ||   |_| |  |  _____||   | |   |    |  |_|  ||  |_|  |",
+"---------,','  |            |       || | |   ||       ||       |  | |_____ |   | |   |    |       ||       |",
+"         ||    |            |       || |_|   ||       ||  _    |  |_____  ||   | |   |___ |       ||       | ",
+"         ||    |            |   _   ||       ||   _   || | |   |   _____| ||   | |       | |     | |   _   |",
+"         ||    |______      |__| |__||______| |__| |__||_|  |__|  |_______||___| |_______|  |___|  |__| |__| ",
+"         ||    |     ,|      ",
+"         ||  ,'    ,' |              Student who is really obsessed with cybersecurity and IT!",
+"         ||,'    ,'   |                   College Freshmen, attending Allan Hancock College",
+"_________|/    ,'     |                    Waitlisted at Cal Poly SLO for Computer Science",
+"_____________,'      ,',              COMPTIA ITF+ Certified, testing for Security+ in July!",
+"             |     ,','     ",
+"             |   ,','    ",
+"             | ,',' ",
+"_____________|','   ",
+"              ",
+  '<span class="color2">You have appeared to reach the terminal.</span>',
+  '<span class="color2">Let me guess. Used a Metasploit module? Dirbuster? A simple nmap scan?</span>',
+  '<span class="color2">Whatever you used to get here, welcome to my site!</span>',
+  '<span class="color2">Here is your terminal you wanted. (not using root of course!) </span>',
+  "<span class=\"color2\">Use the </span> <span class=\"command\">'help'</span><span class=\"color2\"> command to access the avaliable commands!</span>",
+  '<span class="color2">Enjoy your stay!</span>',
+  "<span class=\"color2\">As always,</span> <span class=\"command\"><b>HACK THE PLANET!</b></span><span class=\"color2\"></span>"
+];
